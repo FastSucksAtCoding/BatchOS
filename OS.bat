21fc8ad61af08b7ca8d336e78cad51596ccaf63c
FastSucksAtCoding <68865690+FastSucksAtCoding@users.noreply.github.com>
2021-04-28 15:38:12 +0300

    Create OS.bat

---
+++ OS.bat
@@ -0,0 +1,14 @@
+:: https://github.com/fastsucksatcoding/BatchOS
+
+@echo off
+title BatchOS
+:main
+echo =========================================================================
+echo = BatchOS is open-source system coded in Batch Programming language     =
+echo = BatchOS is developed by single hacker known as InfiniteOnTop          =
+echo =========================================================================
+echo ++ Welcome to BatchOS ++
+echo ::Type help to get command list::
+echo Type a command:
+set /p command="Enter a command:"
+if %command%==help goto help 
