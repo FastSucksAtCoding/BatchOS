ab3a2843108283382a4c31d98b6ebf0ba0b2d1ea
FastSucksAtCoding <68865690+FastSucksAtCoding@users.noreply.github.com>
2021-04-28 15:59:52 +0300

    Update OS.bat

--- OS.bat
+++ OS.bat
@@ -20,3 +20,69 @@ echo help - shows this message
 echo ping - ping an ip
 
 :ping 
+set /p IP="Enter ip to ping:"
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
