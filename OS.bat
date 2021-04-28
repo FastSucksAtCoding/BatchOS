b78ed8b5d1b21981a292d57f5787ee61e5df9ceb
FastSucksAtCoding <68865690+FastSucksAtCoding@users.noreply.github.com>
2021-04-28 16:11:33 +0300

    Update OS.bat

--- OS.bat
+++ OS.bat
@@ -3,29 +3,35 @@
 @echo off
 title BatchOS
 :main
+cls
 echo =========================================================================
 echo = BatchOS is open-source system coded in Batch Programming language     =
 echo = BatchOS is developed by single hacker known as InfiniteOnTop          =
 echo =========================================================================
 echo ++ Welcome to BatchOS ++
 echo ::Type help to get command list::
-echo Type a command:
 set /p command="Enter a command:"
 if %command%==help goto help 
-if %command%==ping goto ping
+if %command%==ping goto png
+if %command%==vrsn goto vrsn 
 
 :help
 cls
 echo help - shows this message 
-echo ping - ping an ip
+echo png - ping an ip
+echo vrsn - shows version of BatchOS
+set /p command1="Enter a command:"
+if %command1%==help goto help 
+if %command1%==ping goto png
+if %command1%==vrsn goto vrsn 
 
-:ping 
+:png 
+cls
 set /p IP="Enter ip to ping:"
 :rainbow
 PING -n 1 %IP% | FIND "TTL="
 IF ERRORLEVEL 1 (SET in=c & echo offline...)
 ping -t 2 0 10 127.0.0.1 >nul
-
 color a
 PING -n 1 %IP% | FIND "TTL="
 IF ERRORLEVEL 1 (SET in=c & echo offline...)
@@ -86,3 +92,11 @@ color 0f
 PING -n 1 %IP% | FIND "TTL="
 IF ERRORLEVEL 1 (SET in=c & echo offline...)
 ping -t 2 0 10 127.0.0.1 >nul
+
+:vrsn
+cls 
+echo BatchOS version is 1.0
+set /p command2="Enter a command:"
+if %command2%==help goto help 
+if %command2%==ping goto png
+if %command2%==vrsn goto vrsn 
